0a4b99956be5046c1ba943eeae23deb21e98a621
Haitham Assoli <74528836+haithamassoli@users.noreply.github.com>
2021-10-12 22:00:54 +0300

    Add files via upload

--- dom-exersices/exercise8.js
+++ dom-exersices/exercise8.js
@@ -1,19 +1,34 @@
 function initGrid() {
-    // collect colors in an array
-    var colors = [];
-    var range = ["00", "33", "66", "99", "cc", "ff"];
+  // collect colors in an array
+  var colors = [];
+  var range = ["00", "33", "66", "99", "cc", "ff"];
 
-    for (var r = 0; r < range.length; r++) {
-        for (var g = 0; g < range.length; g++) {
-            for (var b = 0; b < range.length; b++) {
-                colors.push("#" + range[r] + range[g] + range[b]);
-            }
-        }
+  for (var r = 0; r < range.length; r++) {
+    for (var g = 0; g < range.length; g++) {
+      for (var b = 0; b < range.length; b++) {
+        colors.push("#" + range[r] + range[g] + range[b]);
+      }
     }
+  }
 
-    // TODO complete the rest
+  let colorsPick = document.querySelector("#colors");
+
+  for (let i = 0; i < colors.length; i++) {
+    let color = document.createElement("div");
+    color.className = "choice";
+    color.style.backgroundColor = colors[i];
+    colorsPick.appendChild(color);
+  }
+  let selected = document.querySelector("#selected");
+  let select = document.createElement("div");
+  colorsPick.addEventListener("click", (e) => {
+    select.innerHTML = e.target.style.backgroundColor;
+    selected.appendChild(select);
+    selected.style.backgroundColor = e.target.style.backgroundColor;
+  });
 }
 
-window.onload = function () {
-    initGrid();
-}
+// function colorInfo() {}
+// selected.addEventListener("click", colorInfo);
+// colorsPick.appendChild(aaa);
+initGrid();
